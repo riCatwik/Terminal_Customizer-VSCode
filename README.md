c1622a4b61528d28da66d71d8f6d07bac4ca70bf
Ritwik Das <59201175+riCatwik@users.noreply.github.com>
2023-08-21 21:48:29 +0200

    cmd and powershell added

--- README.md
+++ README.md
@@ -1,35 +1,47 @@
-# PowerShell Profile Customizer
+# Terminal Customizer for VSCode
 
-This VS Code extension customizes your PowerShell profile to change the prompt display. The new prompt displays the current directory on one line, followed by a `>` symbol on the next, making it more visually separated and easier to read.
+This VS Code extension customizes your PowerShell, Windows PowerShell, and Command Prompt profiles to change the prompt display. The new prompt displays the current directory on one line, followed by a `>` symbol on the next, making it more visually separated and easier to read.
 
 ## Features
 
-- Automatically updates the PowerShell profile upon activation.
-- Makes the PowerShell prompt in the integrated terminal more readable.
+- Customizes the profiles for PowerShell, Windows PowerShell, and Command Prompt upon activation.
+- Makes the prompt in the integrated terminal more readable.
 
 ## Requirements
 
-- Windows OS with PowerShell installed.
-- A valid PowerShell profile. If one doesn't exist, the extension will create it.
-
+- Windows OS with PowerShell or Command Prompt installed.
+- A valid PowerShell, Windows PowerShell, or Command Prompt profile. If one doesn't exist, the extension will create it.
+  
 ## How to Use
 
 1. Install the extension from the VS Code Marketplace or by manually adding the `.vsix` file.
-2. Upon activation, the extension will automatically modify the PowerShell profile.
-3. Open a new PowerShell terminal in VS Code to see the changes.
+2. Activate the extension, and it will automatically modify the profiles for PowerShell, Windows PowerShell, and Command Prompt.
+3. Open a new terminal in VS Code to see the changes.
+
+### Special Instructions for Command Prompt Customization
+
+For the Command Prompt customization to take effect, the `AutoRun` key in the Windows Registry needs to be set to execute the `cmdProfile.cmd` located in the user's home directory. Here's how you can do it:
+
+1. Press `Win + R`, type `regedit`, and press Enter to open the Registry Editor.
+2. Navigate to `HKEY_CURRENT_USER\Software\Microsoft\Command Processor`.
+3. Check for an `AutoRun` key. If it doesn't exist, right-click on the right pane, select `New` -> `String Value`, and name it `AutoRun`.
+4. Double-click the `AutoRun` key, and in the `Value data` field, input the path to your `cmdProfile.cmd`. It would typically be something like `C:\Users\<YourUsername>\cmdProfile.cmd`.
+5. Click OK and close the Registry Editor.
+
+Please ensure you're careful while editing the Windows Registry. Making incorrect changes can affect system behavior.
 
 ## Known Issues
 
-- Please make sure to back up your PowerShell profile before using the extension.
-- If you encounter any issues, please report them on the [GitHub repository](https://github.com/riCatwik/PowerShellProfileCustomizer).
+- Please make sure to back up your profiles (PowerShell, Windows PowerShell, Command Prompt) before using the extension.
+- If you encounter any issues, please report them on the [GitHub repository](https://github.com/riCatwik/Terminal_Customizer-VSCode).
 
 ## Feedback
 
-Your feedback is invaluable! If you have any suggestions or run into any issues, please don't hesitate to open an issue on [my GitHub repo](https://github.com/riCatwik/PowerShellProfileCustomizer).
+Your feedback is invaluable! If you have any suggestions or run into any issues, please don't hesitate to open an issue on [my GitHub repo](https://github.com/riCatwik/Terminal_Customizer-VSCode).
 
 ## Disclaimer
 
-This extension modifies the `Microsoft.PowerShell_profile.ps1` file. While care has been taken to ensure the modifications are safe, always ensure you have backups of any important files.
+This extension modifies the `Microsoft.PowerShell_profile.ps1`, Windows PowerShell's `profile.ps1`, and Command Prompt's profile files (`cmdProfile.cmd` in the user's home directory). While care has been taken to ensure the modifications are safe, always ensure you have backups of any important files. Please proceed with caution when editing the Windows Registry as per the instructions above.
 
 ## **Enjoy!**
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,65 +1,104 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-export function activate(context: vscode.ExtensionContext) {
-	let psProfilePath = path.join(process.env.USERPROFILE || '', 'Documents', 'PowerShell', 'Microsoft.PowerShell_profile.ps1');
-	let backupPath = psProfilePath + '.backup';
-	let psProfileDir = path.dirname(psProfilePath);
-
-	// Ensure the directory exists
-	if (!fs.existsSync(psProfileDir)) {
-		fs.mkdirSync(psProfileDir, { recursive: true });
-	}
-
-	if (!fs.existsSync(backupPath)) {
-		if (fs.existsSync(psProfilePath)) {
-			fs.copyFileSync(psProfilePath, backupPath);  // Create a backup before modifying
-		}
-	}
-
-	let newPromptFunction =
-		"function prompt {\n" +
-		"    # Display the current path in green\n" +
-		"    $currentPath = $PWD.Path\n" +
-		"    Write-Host $currentPath -NoNewline -ForegroundColor Green\n" +
-		"    Write-Host \"`n> \" -NoNewline\n" +
-		"    return \" \"\n" +
-		"}";
-
-	if (fs.existsSync(psProfilePath)) {
-		let content = fs.readFileSync(psProfilePath, 'utf8');
-		if (!content.includes('function prompt')) {
-			content += newPromptFunction;
-			fs.writeFileSync(psProfilePath, content, 'utf8');
-		}
-	} else {
-		fs.writeFileSync(psProfilePath, newPromptFunction, 'utf8');
-	}
-
-	vscode.window.showInformationMessage('PowerShell profile customized!');
+interface ProfileInfo {
+	profilePath: string;
+	name: string;
 }
 
+function getProfilePaths(): ProfileInfo[] {
+	let userProfile = process.env.USERPROFILE || '';
+	return [
+		{
+			profilePath: path.join(userProfile, 'Documents', 'WindowsPowerShell', 'profile.ps1'),
+			name: 'Windows PowerShell'
+		},
+		{
+			profilePath: path.join(userProfile, 'cmdProfile.cmd'), // Directly in the user's home directory
+			name: 'Command Prompt'
+		},
+		{
+			profilePath: path.join(userProfile, 'Documents', 'PowerShell', 'Microsoft.PowerShell_profile.ps1'),
+			name: 'PowerShell'
+		}
+	];
+}
 
+export function activate(context: vscode.ExtensionContext) {
+	let profiles = getProfilePaths();
 
+	profiles.forEach(paths => {
+		let psProfilePath = paths.profilePath;
+		let backupPath = psProfilePath + '.backup';
+		let psProfileDir = path.dirname(psProfilePath);
 
-// This method is called when your extension is deactivated
-export function deactivate() {
-	let psProfilePath = path.join(process.env.USERPROFILE || '', 'Documents', 'PowerShell', 'Microsoft.PowerShell_profile.ps1');
-	let backupPath = psProfilePath + '.backup';
+		// Ensure the directory exists
+		if (!fs.existsSync(psProfileDir)) {
+			fs.mkdirSync(psProfileDir, { recursive: true });
+		}
 
-	// Check if backup exists
-	if (fs.existsSync(backupPath)) {
-		// Ask the user if they want to restore their original profile
-		vscode.window.showInformationMessage('Do you want to restore your original PowerShell profile?', 'Yes', 'No').then(selection => {
-			if (selection === 'Yes') {
-				fs.copyFileSync(backupPath, psProfilePath);  // Restore from backup
-				fs.unlinkSync(backupPath);  // Delete the backup after restoring
-				vscode.window.showInformationMessage('Original PowerShell profile restored.');
+		// Backup logic
+		if (!fs.existsSync(backupPath)) {
+			if (fs.existsSync(psProfilePath)) {
+				fs.copyFileSync(psProfilePath, backupPath);
 			}
-		});
-	}
+		}
+
+		let newPromptFunction: string = ''; // Set default value to an empty string
+
+		switch (paths.name) {
+			case 'Windows PowerShell':
+			case 'PowerShell':
+				newPromptFunction =
+					"function prompt {\n" +
+					"    $currentPath = $PWD.Path\n" +
+					"    Write-Host $currentPath -NoNewline -ForegroundColor Green\n" +
+					"    Write-Host \"`n> \" -NoNewline\n" +
+					"    return \" \"\n" +
+					"}";
+				break;
+			case 'Command Prompt':
+				newPromptFunction =
+					"@echo off\n" +
+					"echo | set /p nul=%cd%\n" +
+					"echo ^>\n";
+				break;
+		}
+
+		if (fs.existsSync(psProfilePath)) {
+			let content = fs.readFileSync(psProfilePath, 'utf8');
+			if (!content.includes('function prompt') && (paths.name === 'Windows PowerShell' || paths.name === 'PowerShell')) {
+				content += newPromptFunction;
+				fs.writeFileSync(psProfilePath, content, 'utf8');
+			} else if (!content.includes('@echo off') && paths.name === 'Command Prompt') {
+				content += newPromptFunction;
+				fs.writeFileSync(psProfilePath, content, 'utf8');
+			}
+		} else {
+			fs.writeFileSync(psProfilePath, newPromptFunction, 'utf8');
+		}
+
+		vscode.window.showInformationMessage(`${paths.name} profile customized!`);
+	});
+}
+
+export function deactivate() {
+	let profiles = getProfilePaths();
+
+	profiles.forEach(paths => {
+		let psProfilePath = paths.profilePath;
+		let backupPath = psProfilePath + '.backup';
+
+		// Check if backup exists
+		if (fs.existsSync(backupPath)) {
+			vscode.window.showInformationMessage(`Do you want to restore your original ${paths.name} profile?`, 'Yes', 'No').then(selection => {
+				if (selection === 'Yes') {
+					fs.copyFileSync(backupPath, psProfilePath);
+					fs.unlinkSync(backupPath);
+					vscode.window.showInformationMessage(`Original ${paths.name} profile restored.`);
+				}
+			});
+		}
+	});
 }
